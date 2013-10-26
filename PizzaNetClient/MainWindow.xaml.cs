2a510cff8f87f9cb2fca2c830fe46fc52f1c3434
Maciek <maciejdlus@wp.pl>
2013-10-26 11:02:34 +0200

    first commit

---
+++ PizzaNetClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaNetClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class PizzaNetWindow : Window
+    {
+        public PizzaNetWindow()
+        {
+            InitializeComponent();
+            this.IngredientsCollection = new ObservableCollection<PizzaNetControls.IngredientsRow>();
+            var c = new PizzaNetControls.IngredientsRow();
+            var tb = new TextBlock();
+            tb.Text="lololol";
+            c.Children.Add(tb);
+            this.IngredientsCollection.Add(c);
+            this.DataContext = IngredientsCollection;
+        }
+
+        public ObservableCollection<PizzaNetControls.IngredientsRow> IngredientsCollection;
+    }
+}
---
+++ PizzaNetControls/IngredientsRow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaNetControls
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    [ContentProperty("Children")]
+    public partial class IngredientsRow : UserControl
+    {
+        public IngredientsRow()
+        {
+            InitializeComponent();
+            Children = PART_Host.Children;
+        }
+
+        public static readonly DependencyPropertyKey ChildrenProperty = DependencyProperty.RegisterReadOnly(
+            "Children",
+            typeof(UIElementCollection),
+            typeof(IngredientsRow),
+            new PropertyMetadata());
+
+        public UIElementCollection Children
+        {
+            get { return (UIElementCollection)GetValue(ChildrenProperty.DependencyProperty); }
+            private set { SetValue(ChildrenProperty, value); }
+        }
+    }
+}
