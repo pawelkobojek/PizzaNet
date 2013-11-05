a7d6212b6fb8c9a2744528794463fad58d049288
Maciek <maciejdlus@wp.pl>
2013-11-05 22:32:08 +0100

    worker control

---
+++ PizzaNetControls/Worker/WorkerControl.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaNetControls.Worker
+{
+    /// <summary>
+    /// Interaction logic for WorkerControl.xaml
+    /// </summary>
+    public partial class WorkerControl : UserControl
+    {
+        private Queue<WorkerTask> tasks;
+        private BackgroundWorker worker;
+
+        public Control Lock { get; set; }
+
+        public WorkerControl()
+        {
+            InitializeComponent();
+            this.tasks = new Queue<WorkerTask>();
+
+            this.worker = new BackgroundWorker();
+            this.worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+        }
+
+        public void EnqueueTask(WorkerTask task)
+        {
+            tasks.Enqueue(task);
+            if (!worker.IsBusy)
+            {
+                nextTask();
+            }
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (tasks.Count == 0)
+            {
+                this.spinner.Visibility = System.Windows.Visibility.Hidden;
+                if (this.Lock != null)
+                    this.Lock.IsEnabled = true;
+            }
+            else nextTask();
+        }
+
+        void nextTask()
+        {
+            this.spinner.Visibility = System.Windows.Visibility.Visible;
+            if (this.Lock != null)
+                this.Lock.IsEnabled = false;
+            WorkerTask t = tasks.Dequeue();
+            worker.DoWork += (s, e) =>
+            {
+                t.Result = t.Task(t.Arguments);
+            };
+            worker.RunWorkerCompleted += (s, e) =>
+            {
+                t.Handler(this, new WorkFinishedEventArgs(t.Result, t.Arguments));
+            };
+            worker.RunWorkerAsync();
+        }
+    }
+}
---
+++ PizzaNetControls/Worker/WorkerTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaNetControls.Worker
+{
+    public class WorkerTask
+    {
+        public delegate object RunnableTask(object[] args);
+        public delegate void WorkFinishedHandler(object sender, WorkFinishedEventArgs e);
+
+        public WorkerTask()
+        {
+        }
+
+        public WorkerTask(RunnableTask task, WorkFinishedHandler handler, params object[] args)
+        {
+            this.Task = task;
+            this.Handler = handler;
+            this.Arguments = args;
+        }
+
+        public RunnableTask Task { get; set; }
+        public WorkFinishedHandler Handler { get; set; }
+        public object[] Arguments { get; set; }
+        public object Result { get; set; }
+    }
+}
